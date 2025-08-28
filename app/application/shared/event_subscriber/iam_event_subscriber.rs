55cf3b041f4a39d54be4e903bf30bfd86a1ae6a2
shmy <chao.w1226@gmail.com>
2025-08-28 17:09:00 +0800

    updated

--- app/application/shared/event_subscriber/iam_event_subscriber.rs
+++ app/application/shared/event_subscriber/iam_event_subscriber.rs
@@ -78,66 +78,64 @@ where
     T: PermissionResolver + Clone + Debug + Send + Sync + 'static,
 {
     async fn on_received(&self, event: Event) -> anyhow::Result<()> {
-        match event {
-            Event::Iam(e) => {
-                if Self::is_permission_changed(&e)
-                    && let Err(err) = self.permission_resolver.refresh().await
-                {
-                    tracing::error!(?e, error = %err, "权限刷新失败");
-                }
-                if Self::is_users_changed(&e) {
-                    self.search_user_query_handler.clean_cache();
-                }
-                if Self::is_roles_changed(&e) {
-                    self.search_role_query_handler.clean_cache();
-                }
-                match e {
-                    IamEvent::UsersCreated { items } => {
-                        let paths = items
-                            .into_iter()
-                            .filter_map(|item| item.portrait.clone())
-                            .collect::<Vec<_>>();
-                        if let Err(err) = self.file_service.set_files_used(&paths).await {
-                            tracing::error!(error = %err, "UsersCreated: failed to set files used");
-                        }
+        if let Event::Iam(e) = event {
+            if Self::is_permission_changed(&e)
+                && let Err(err) = self.permission_resolver.refresh().await
+            {
+                tracing::error!(?e, error = %err, "权限刷新失败");
+            }
+            if Self::is_users_changed(&e) {
+                self.search_user_query_handler.clean_cache();
+            }
+            if Self::is_roles_changed(&e) {
+                self.search_role_query_handler.clean_cache();
+            }
+            match e {
+                IamEvent::UsersCreated { items } => {
+                    let paths = items
+                        .into_iter()
+                        .filter_map(|item| item.portrait.clone())
+                        .collect::<Vec<_>>();
+                    if let Err(err) = self.file_service.set_files_used(&paths).await {
+                        tracing::error!(error = %err, "UsersCreated: failed to set files used");
                     }
-                    IamEvent::UsersUpdated { items } => {
-                        let (mut unused_paths, mut used_paths) = (Vec::new(), Vec::new());
+                }
+                IamEvent::UsersUpdated { items } => {
+                    let (mut unused_paths, mut used_paths) = (Vec::new(), Vec::new());
 
-                        for item in items {
-                            match (&item.before.portrait, &item.after.portrait) {
-                                (Some(before), Some(after)) if before != after => {
-                                    // 用户修改了头像
-                                    unused_paths.push(before.clone());
-                                    used_paths.push(after.clone());
-                                }
-                                (Some(before), None) => {
-                                    unused_paths.push(before.clone()); // 用户删除了头像
-                                }
-                                (None, Some(after)) => {
-                                    used_paths.push(after.clone()); // 用户新增了头像
-                                }
-                                _ => {} // 没有变化的情况，不操作
+                    for item in items {
+                        match (&item.before.portrait, &item.after.portrait) {
+                            (Some(before), Some(after)) if before != after => {
+                                // 用户修改了头像
+                                unused_paths.push(before.clone());
+                                used_paths.push(after.clone());
                             }
-                        }
-                        if let Err(err) = self.file_service.set_files_unused(&unused_paths).await {
-                            tracing::error!(error = %err, "UsersUpdated: failed to set files unused");
-                        }
-                        if let Err(err) = self.file_service.set_files_used(&used_paths).await {
-                            tracing::error!(error = %err, "UsersUpdated: failed to set files used");
-                        }
-                    }
-                    IamEvent::UsersDeleted { items } => {
-                        let paths = items
-                            .into_iter()
-                            .filter_map(|item| item.portrait.clone())
-                            .collect::<Vec<_>>();
-                        if let Err(err) = self.file_service.set_files_unused(&paths).await {
-                            tracing::error!(error = %err, "UsersDeleted: failed to set files unused");
+                            (Some(before), None) => {
+                                unused_paths.push(before.clone()); // 用户删除了头像
+                            }
+                            (None, Some(after)) => {
+                                used_paths.push(after.clone()); // 用户新增了头像
+                            }
+                            _ => {} // 没有变化的情况，不操作
                         }
                     }
-                    _ => {}
+                    if let Err(err) = self.file_service.set_files_unused(&unused_paths).await {
+                        tracing::error!(error = %err, "UsersUpdated: failed to set files unused");
+                    }
+                    if let Err(err) = self.file_service.set_files_used(&used_paths).await {
+                        tracing::error!(error = %err, "UsersUpdated: failed to set files used");
+                    }
                 }
+                IamEvent::UsersDeleted { items } => {
+                    let paths = items
+                        .into_iter()
+                        .filter_map(|item| item.portrait.clone())
+                        .collect::<Vec<_>>();
+                    if let Err(err) = self.file_service.set_files_unused(&paths).await {
+                        tracing::error!(error = %err, "UsersDeleted: failed to set files unused");
+                    }
+                }
+                _ => {}
             }
         }
         Ok(())
