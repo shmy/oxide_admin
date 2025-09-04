edeeb642f656286555c54754844e4e370f8742e8
shmy <chao.w1226@gmail.com>
2025-09-04 21:00:17 +0800

    updated

--- app/infrastructure/migration/mod.rs
+++ app/infrastructure/migration/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use anyhow::Result;
 use domain::iam::entity::user::User;
 use domain::iam::value_object::hashed_password::HashedPassword;
@@ -8,26 +10,17 @@ use tracing::info;
 
 use crate::repository::iam::role_repository::RoleRepositoryImpl;
 use crate::repository::iam::user_repository::UserRepositoryImpl;
-use crate::shared::kv::{Kv, KvTrait as _};
 use crate::shared::pg_pool::PgPool;
 use crate::shared::provider::Provider;
 
-const INSERT_USER_ROLE_KEY: &str = "insert_user_role";
-
 pub async fn migrate(provider: &Provider) -> Result<()> {
     let pool = provider.provide::<PgPool>();
-    let kv = provider.provide::<Kv>();
     sqlx::migrate!("./migration/sql").run(&pool).await?;
-    insert_user_role(&pool, &kv, provider).await?;
+    insert_user_role(&pool, provider).await?;
     Ok(())
 }
 
-async fn insert_user_role(pool: &PgPool, kv: &Kv, provider: &Provider) -> Result<()> {
-    let inserted = kv.get::<bool>(INSERT_USER_ROLE_KEY).unwrap_or_default();
-    if inserted {
-        info!("User role already inserted");
-        return Ok(());
-    }
+async fn insert_user_role(pool: &PgPool, provider: &Provider) -> Result<()> {
     let (role_opt, user_opt) = tokio::try_join!(
         sqlx::query!("SELECT id from _roles WHERE privileged = true").fetch_optional(pool),
         sqlx::query!("SELECT id from _users WHERE privileged = true").fetch_optional(pool),
@@ -44,7 +37,7 @@ async fn insert_user_role(pool: &PgPool, kv: &Kv, provider: &Provider) -> Result
             .permission_ids(vec![])
             .build();
         let role = role_repository.save(role).await?;
-
+        info!("Role inserted: {}", role.id.deref());
         if user_opt.is_none() {
             let user_repository = provider.provide::<UserRepositoryImpl>();
             let user = User::builder()
@@ -56,9 +49,9 @@ async fn insert_user_role(pool: &PgPool, kv: &Kv, provider: &Provider) -> Result
                 .privileged(true)
                 .role_ids(vec![role.id.clone()])
                 .build();
-            user_repository.save(user).await?;
+            let user = user_repository.save(user).await?;
+            info!("User inserted: {}", user.id.deref());
         }
     }
-    kv.set::<bool>(INSERT_USER_ROLE_KEY, true)?;
     Ok(())
 }
