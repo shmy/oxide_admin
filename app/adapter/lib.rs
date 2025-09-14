46898d1d15d4a377d41d590cdb0b7628b97bd1bc
shmy <chao.w1226@gmail.com>
2025-09-14 10:45:17 +0800

    add health check endpoint

--- app/adapter/lib.rs
+++ app/adapter/lib.rs
@@ -4,6 +4,8 @@ mod frontend;
 mod shared;
 mod upload;
 use axum::Router;
+use axum::http::StatusCode;
+use axum::routing::get;
 pub use shared::constant::*;
 pub use shared::state::*;
 
@@ -11,8 +13,12 @@ pub fn routing(state: WebState) -> Router {
     let router = Router::new()
         .nest("/api", api::routing(state.clone()))
         .with_state(state.clone())
-        .merge(upload::routing(state));
+        .merge(upload::routing(state))
+        .route("/health", get(health));
     #[cfg(not(debug_assertions))]
     let router = router.merge(frontend::routing());
     router
+}
+async fn health() -> StatusCode {
+    StatusCode::OK
 }
