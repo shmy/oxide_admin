1d34a7ec88231f06414aa3906066e9d5ab9e4d4d
shmy <chao.w1226@gmail.com>
2025-09-20 14:57:24 +0800

    add tests

--- app/application/shared/scheduler_job/cleanup_unused_file.rs
+++ app/application/shared/scheduler_job/cleanup_unused_file.rs
@@ -1,11 +1,12 @@
 use anyhow::Result;
+use bon::Builder;
 use futures_util::StreamExt as _;
 use nject::injectable;
 use sched_kit::ScheduledJob;
 
 use crate::system::service::{file_service::FileService, upload_service::UploadService};
 
-#[derive(Clone)]
+#[derive(Clone, Builder)]
 #[injectable]
 pub struct CleanupUnusedFile {
     file_service: FileService,
@@ -45,5 +46,68 @@ impl ScheduledJob for CleanupUnusedFile {
             }
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use domain::shared::id_generator::IdGenerator;
+    use infrastructure::{
+        shared::{
+            chrono_tz::{ChronoTz, Duration},
+            pg_pool::PgPool,
+            workspace::WorkspaceRef,
+        },
+        test_utils::{setup_database, setup_object_storage},
+    };
+    use sqlx::types::chrono::Utc;
+
+    use super::*;
+
+    async fn build_job(pool: PgPool) -> CleanupUnusedFile {
+        setup_database(pool.clone()).await;
+        let object_storage = setup_object_storage().await;
+        let file_service = FileService::builder()
+            .pool(pool.clone())
+            .ct(ChronoTz::default())
+            .build();
+        let upload_service = {
+            let file_service = {
+                FileService::builder()
+                    .pool(pool.clone())
+                    .ct(ChronoTz::default())
+                    .build()
+            };
+            UploadService::builder()
+                .ct(ChronoTz::default())
+                .object_storage(object_storage)
+                .file_service(file_service)
+                .workspace(WorkspaceRef::default())
+                .build()
+        };
+        CleanupUnusedFile::builder()
+            .file_service(file_service)
+            .upload_service(upload_service)
+            .build()
+    }
+
+    #[sqlx::test]
+    async fn test_cleanup_unused_files(pool: PgPool) {
+        let job = build_job(pool.clone()).await;
+        assert!(job.run().await.is_ok());
+        // Insert a 2-day-old file
+        let now = Utc::now() - Duration::days(3);
+        let id = IdGenerator::primary_id();
+        let insert = sqlx::query("INSERT INTO _files (id, path, used, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)")
+            .bind(id)
+            .bind("/test/test3.txt")
+            .bind(false)
+            .bind(now)
+            .bind(now)
+            .execute(&pool)
+            .await;
+        assert!(insert.is_ok());
+        assert!(job.run().await.is_ok());
     }
 }
